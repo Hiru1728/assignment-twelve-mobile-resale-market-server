1fe7df44ae5a51c3afa8d2929717eafc49d57f57
Hiru1728 <hirucse1728@gmail.com>
2022-11-27 19:27:06 +0600

    connect to the mongodb

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yrpb3ds.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', async (req, res) => {
     res.send('second hand mobile portal server is running');
