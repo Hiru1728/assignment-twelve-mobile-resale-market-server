409ba263cae93599562ec3450c941a27c753841a
Hiru1728 <hirucse1728@gmail.com>
2022-12-01 20:01:03 +0600

    get booking data from mongodb

--- index.js
+++ index.js
@@ -34,7 +34,14 @@ async function run() {
             res.send(mobile);
         });
 
-
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = {
+                email: email
+            }
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
